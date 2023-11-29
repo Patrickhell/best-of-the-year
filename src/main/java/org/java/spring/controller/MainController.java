a7b290f6fa530e94145ae3462dff929448205ecc
Patrickhell <patrickhell19@yahoo.fr>
2023-11-29 20:17:24 +0100

    add abstract class and links to navigate in my little application

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -2,12 +2,13 @@ package org.java.spring.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.java.spring.pojo.Movie;
+import org.java.spring.pojo.Song;
 
 
 @Controller
@@ -23,102 +24,30 @@ public class MainController {
 		return "index";
 	}
 	
-	public class Movie {
-		
-		int id;
-		String title;
-		
-		public Movie(int id,String title) {
-			
-			setId(id);
-			setTitle(title);
-		}
-
-		public int getId() {
-			return id;
-		}
-
-		public void setId(int id) {
-			this.id = id;
-		}
-
-		public String getTitle() {
-			return title;
-		}
-
-		public void setTitle(String title) {
-			this.title = title;
-		}
-		
-		@Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return "Movie => " + getTitle();
-		}
-	}
-	
-    public class Song {
-		
-		int id;
-		String title;
-		
-        public Song(int id,String title) {
-			
-			setId(id);
-			setTitle(title);
-		}
-    
-        public int getId() {
-		   return id;
-	    }  
-
-	    public void setId(int id) {
-		   this.id = id;
-	    }
-
-	    public String getTitle() {
-		   return title;
-	    }
-
-	    public void setTitle(String title) {
-		   this.title = title;
-	    }
-	    
-	    @Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return "Song => " + getTitle();
-		}
-	}
     
     @GetMapping("/movies")
 	private String getBestMovies(Model model) {
-    	
-    	
 		
-		List<Movie> movie = new ArrayList<>();
+		List<Movie> movies = new ArrayList<>();
 		
-		movie.add(new Movie(1, "Alice au pays des merveilles"));
-		movie.add(new Movie(2, "Superman"));
-		movie.add(new Movie(3, "hello World"));
-		 String moviesList = movie.stream().map(Movie::getTitle).collect(Collectors.joining(" , "));
+		movies.add(new Movie(1, "Alice au pays des merveilles"));
+		movies.add(new Movie(2, "Superman"));
+		movies.add(new Movie(3, "hello World"));
 		 
-		model.addAttribute("moviesList" , moviesList);
+		model.addAttribute("movies" , movies);
 		return "movies" ;
 	}
     
     
     @GetMapping("/songs")
 	private String getBestSongs(Model model) {
-        List<Song> song = new ArrayList<>();
+        List<Song> songs = new ArrayList<>();
 		
-        song.add(new Song(1, "All limit"));
-        song.add(new Song(2, "On fire"));
-        song.add(new Song(3, "Best song"));
-        
-        String songsList = song.stream().map(Song::getTitle).collect(Collectors.joining( " , "));
-		
-		model.addAttribute("songsList" , songsList);
+        songs.add(new Song(1, "All limit"));
+        songs.add(new Song(2, "On fire"));
+        songs.add(new Song(3, "Best song"));
+        		
+		model.addAttribute("songs" , songs);
 		
 		return "songs" ;
 		

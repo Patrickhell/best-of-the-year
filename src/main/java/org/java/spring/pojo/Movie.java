97712dbd138041e6640e7556829138e7b75c4d50
Patrickhell <patrickhell19@yahoo.fr>
2023-11-29 20:22:31 +0100

    first commit

---
+++ src/main/java/org/java/spring/pojo/Movie.java
@@ -0,0 +1,12 @@
+package org.java.spring.pojo;
+
+import org.java.spring.pojo.abs.Media;
+
+public class Movie  extends Media {
+	
+
+public Movie(int id, String title) {
+		
+		super(id, title);
+	}	
+}
---
+++ src/main/java/org/java/spring/pojo/Song.java
@@ -0,0 +1,12 @@
+package org.java.spring.pojo;
+
+import org.java.spring.pojo.abs.Media;
+
+public class Song extends Media {
+
+	
+    public Song(int id, String title) {
+		
+		super(id, title);
+	}	
+}
